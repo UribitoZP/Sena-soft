438e6f5c1136adc968dbdcd1bc54235f0039b317
lumendozu <mendozalucho826@gmail.com>
2026-04-04 10:16:53 -0500

    corregir run.bat: agregar sqlite al classpath y compilar todos los fuentes

--- Aplicativo Escritorio/run.bat
+++ Aplicativo Escritorio/run.bat
@@ -6,16 +6,39 @@ set "BIN_DIR=bin"
 set "LIB_DIR=lib"
 set "RESOURCES_DIR=resources"
 set "MAIN_CLASS=com.santaana.main.ApplicationMain"
+set "CLASSPATH=.;..\%LIB_DIR%\jcalendar-1.4.jar;..\%LIB_DIR%\sqlite-jdbc-3.45.1.0.jar"
 
 echo [+] Creando directorio de binarios...
 if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
 
 echo [+] Compilando proyecto...
-javac -cp ".;%LIB_DIR%\jcalendar-1.4.jar" -d "%BIN_DIR%" -sourcepath "%SRC_DIR%" ^
+javac -cp ".;%LIB_DIR%\jcalendar-1.4.jar;%LIB_DIR%\sqlite-jdbc-3.45.1.0.jar" ^
+    -d "%BIN_DIR%" ^
+    -sourcepath "%SRC_DIR%" ^
     "%SRC_DIR%\com\santaana\main\ApplicationMain.java" ^
+    "%SRC_DIR%\com\santaana\controller\LoginController.java" ^
+    "%SRC_DIR%\com\santaana\db\DatabaseConnection.java" ^
+    "%SRC_DIR%\com\santaana\db\SchemaManager.java" ^
+    "%SRC_DIR%\com\santaana\db\SeedData.java" ^
+    "%SRC_DIR%\com\santaana\model\Usuario.java" ^
+    "%SRC_DIR%\com\santaana\model\Habitacion.java" ^
+    "%SRC_DIR%\com\santaana\model\Reserva.java" ^
+    "%SRC_DIR%\com\santaana\dao\UsuarioDAO.java" ^
+    "%SRC_DIR%\com\santaana\dao\HabitacionDAO.java" ^
+    "%SRC_DIR%\com\santaana\dao\ReservaDAO.java" ^
+    "%SRC_DIR%\com\santaana\util\ThemeManager.java" ^
     "%SRC_DIR%\com\santaana\view\LoginFrame.java" ^
-    "%SRC_DIR%\com\santaana\view\HomeFrame.java" ^
-    "%SRC_DIR%\com\santaana\controller\LoginController.java"
+    "%SRC_DIR%\com\santaana\view\MainFrame.java" ^
+    "%SRC_DIR%\com\santaana\view\TableroPanel.java" ^
+    "%SRC_DIR%\com\santaana\view\TableroFrame.java" ^
+    "%SRC_DIR%\com\santaana\view\ReservaPanel.java" ^
+    "%SRC_DIR%\com\santaana\view\ReservaFrame.java" ^
+    "%SRC_DIR%\com\santaana\view\GestHabitacionPanel.java" ^
+    "%SRC_DIR%\com\santaana\view\GestHabitacion.java" ^
+    "%SRC_DIR%\com\santaana\view\NotificacionPanel.java" ^
+    "%SRC_DIR%\com\santaana\view\NotificacionFrame.java" ^
+    "%SRC_DIR%\com\santaana\view\NuevaReservaDialog.java" ^
+    "%SRC_DIR%\com\santaana\view\InfoHabitacionFrame.java"
 
 if %errorlevel% neq 0 (
     echo [!] Error de compilacion.
@@ -25,9 +48,10 @@ if %errorlevel% neq 0 (
 
 echo [+] Copiando recursos...
 xcopy /s /e /y "%RESOURCES_DIR%" "%BIN_DIR%\resources\" >nul 2>&1
+xcopy /s /e /y "%SRC_DIR%\main\resources\resources\*" "%BIN_DIR%\resources\" >nul 2>&1
 
 echo [+] Iniciando aplicacion...
 cd "%BIN_DIR%"
-java -cp ".;..\%LIB_DIR%\jcalendar-1.4.jar" "%MAIN_CLASS%"
+java -cp "%CLASSPATH%" "%MAIN_CLASS%"
 
 endlocal
